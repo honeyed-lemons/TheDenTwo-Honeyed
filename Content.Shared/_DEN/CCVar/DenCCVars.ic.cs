af815a5e529dbe4be010b9568d5747d661f6a4f4
honeyed-lemons <alex91905@yahoo.com>
2026-04-11 21:55:14 -0400

    disable random height by default

--- Content.Shared/_DEN/CCVar/DenCCVars.ic.cs
+++ Content.Shared/_DEN/CCVar/DenCCVars.ic.cs
@@ -8,5 +8,5 @@ public sealed partial class DenCCVars
     ///     Whether to randomize height when randomizing a character.
     /// </summary>
     public static readonly CVarDef<bool> RandomizeHeight =
-        CVarDef.Create("ic.random_height", f, CValsear.SERVER | CVar.REPLICATED);
+        CVarDef.Create("ic.random_height", false, CVar.SERVER | CVar.REPLICATED);
 }
